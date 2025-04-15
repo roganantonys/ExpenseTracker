99014252ff84bc6e4db6b869423741a56ccd68bc
rogan_antony <rogan@ysquaretechnology.com>
2025-04-15 22:09:17 +0530

    Add: Screens Skeleton created

---
+++ src/RecentExpanses.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RecentExpanses = () => {
+  return (
+    <View>
+      <Text>RecentExpanses</Text>
+    </View>
+  )
+}
+
+export default RecentExpanses
+
+const styles = StyleSheet.create({})
